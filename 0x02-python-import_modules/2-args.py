3771115a2f65fd725ed7a3cdec190c98f7f0aee5
kiplrry <kiplarry19@gmail.com>
2023-08-18 21:23:35 +0300

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    print(f"{len(argv) - 1} arguments:")
+    if len(argv) - 1 == 1:
+        print(f"{len(argv) - 1} argument:")
+    else:
+        print(f"{len(argv) - 1} arguments:")
     for i in range(1, len(argv)):
         print(f"{i}: {argv[i]}")
