595ba63ad9a51e11bd9fbd99837a0c98e47a8367
kiplrry <kiplarry19@gmail.com>
2023-09-05 22:39:04 +0300

    m

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    def print_matrix_integer(matrix=[[]]):
-        if matrix == [[]]:
-            print("$")
-        for i in matrix:
-            for j in i:
-                if i.index(j) == len(i) - 1:
-                    print(f"{j}$")
-                else:
-                    print(j, end=" ")
+def print_matrix_integer(matrix=[[]]):
+    if matrix == [[]]:
+        print("")
+    for i in matrix:
+        for j in i:
+            if i.index(j) == len(i) - 1:
+                print(f"{j}")
+            else:
+                print(j, end=" ")
