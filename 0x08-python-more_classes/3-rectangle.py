f359d88285a1aaf7b2db6760ac2d6ce1b87d1993
kiplrry <kiplarry19@gmail.com>
2023-09-08 14:13:51 +0300

    3-rectangle.py

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -54,5 +54,6 @@ class Rectangle:
         for i in range(self.__height):
             for j in range(self.__width):
                 st += "#"
-            st += "\n"
+            if i != self.__height - 1:
+                st += "\n"
         return st
