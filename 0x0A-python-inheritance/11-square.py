dc77d0814a47e29b0092924053543a22e9a710c9
kiplrry <kiplarry19@gmail.com>
2023-09-13 20:07:47 +0300

    11-square.py

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+class squre inheriting from rectangle
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Class square
+    """
+    def __init__(self, size):
+        if self.integer_validator("size", size):
+            self.__size = size
+            super().__init__(self.__size, self.__size)
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__size)
