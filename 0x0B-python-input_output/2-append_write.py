688bf9b7a0a66d376af0d42c1fba9ac685fd0961
kiplrry <kiplarry19@gmail.com>
2023-09-14 20:39:04 +0300

    all

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -5,5 +5,6 @@ function that appends to a file
 
 
 def append_write(filename="", text=""):
+    """ function """
     with open(filename, "a", encoding="UTF*") as file:
         return file.write(text)
