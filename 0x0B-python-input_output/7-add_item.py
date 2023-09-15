7b943d2ed48fa5679f5267da726733afa8e643a5
kiplrry <kiplarry19@gmail.com>
2023-09-15 20:42:23 +0300

    l

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,19 +8,11 @@ save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 
-def adder():
-    """function"""
-    j_file = "add_item.json"
-    fp = Path(j_file)
-    if not Path.exists(fp):
-        print("creating")
-        fp.write_text(str([]))
-    loaded_json = load_from_json_file(j_file)
-    print("loaded: " + str(loaded_json))
-    new_json = loaded_json + sys.argv[1:]
-    print("new: " + str(new_json))
-    save_to_json_file(new_json, j_file)
-
-
-if __name__ == "__main__":
-    adder()
+"""function"""
+j_file = "add_item.json"
+fp = Path(j_file)
+if not Path.exists(fp):
+    fp.write_text(str([]))
+loaded_json = load_from_json_file(j_file)
+new_json = loaded_json + sys.argv[1:]
+save_to_json_file(new_json, j_file)
