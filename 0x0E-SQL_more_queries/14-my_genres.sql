f4dfd46b9d252193c23fe268b227739261d85bd4
kiplrry <kiplarry19@gmail.com>
2023-10-19 00:25:14 +0300

    14

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -10,4 +10,5 @@ JOIN tv_shows
 ON tv_show_genres.show_id = tv_shows.id  
 JOIN tv_genres 
 ON tv_genres.id = tv_show_genres.genre_id 
-WHERE tv_shows.title = 'Dexter';
+WHERE tv_shows.title = 'Dexter'
+ORDER BY tv_genres.name;
