7807b063294fe161ea5e3b9d8bf1aca7263ebddc
kiplrry <kiplarry19@gmail.com>
2023-11-13 19:47:50 +0300

    10-factorial.js

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const factnum = parseInt(+process.argv[2]);
+
+function factorial (num) {
+  if(num < 0) {
+    return
+  }
+  if (num === 1 || num === 0 || isNaN(num)) {
+    return 1;
+  }
+  return num * factorial(--num);
+}
+fact = factorial(factnum);
+if(fact){
+  console.log(fact)
+}
