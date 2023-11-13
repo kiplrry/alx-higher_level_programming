5ca5957b20926e4f2a16ac24dbd889f15eca28c5
kiplrry <kiplarry19@gmail.com>
2023-11-13 23:14:55 +0300

    13-add.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,18 +1,21 @@
 #!/usr/bin/node
 const arr = process.argv.slice(2);
-// h.sort()
 const newArr = [];
-arr.forEach((item, index) => {
-  newArr[index] = parseInt(+item);
-});
-const compareFn = function (a, b) {
-  if (a > b) {
-    return 1;
-  } else if (a < b) {
-    return -1;
-  }
-  return 0;
-};
-const sortedArr = newArr.sort(compareFn);
+if (arr.length < 2) {
+  console.log(0);
+} else {
+  arr.forEach((item, index) => {
+    newArr[index] = parseInt(+item);
+  });
+  const compareFn = function (a, b) {
+    if (a > b) {
+      return 1;
+    } else if (a < b) {
+      return -1;
+    }
+    return 0;
+  };
+  const sortedArr = newArr.sort(compareFn);
 
-console.log(sortedArr[sortedArr.length - 2]);
+  console.log(sortedArr[sortedArr.length - 2]);
+}
