d367f901d2da583da166aff3997bae3f89665b37
kiplrry <kiplarry19@gmail.com>
2023-11-17 11:18:24 +0300

    2-my_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+This is a scipt to get states from a db
+"""
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    """
+    The script
+    """
+
+    args = sys.argv
+    details = {
+        'host': 'localhost',
+        'port': 3306,
+        'user': args[1],
+        'password': args[2],
+        'db': args[3]
+    }
+    db = MySQLdb.connect(**details)
+    cur = db.cursor()
+    print(args[4])
+    cur.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC",
+                (args[4],))  # should be a tuple
+    res = cur.fetchall()
+    for row in res:
+        print(row)
+    cur.close()
+    db.close()
