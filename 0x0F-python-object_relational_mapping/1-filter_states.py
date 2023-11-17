88e8312e9b5d80acd0e6d07782b39ba60307bc8d
kiplrry <kiplarry19@gmail.com>
2023-11-17 10:51:31 +0300

    1-filter_states.py

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+This is a scipt to get states from a db
+"""
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    """
+    The script
+    """
+
+    args = sys.argv
+    details = {
+        'host': 'localhost',
+        'port': 3306,
+        'user': args[1],
+        'password': args[2],
+        'db': args[3]
+    }
+    db = MySQLdb.connect(**details)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    res = cur.fetchall()
+    for row in res:
+        print(row)
+    cur.close()
+    db.close()
