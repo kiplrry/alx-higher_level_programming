d9bc3395c61ee3af15c08583cd4a281d0eca5787
root <root@fendo>
2023-11-19 16:30:27 +0300

    6-model_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 '''Creating a class State'''
-from datetime import datetime
-from sqlalchemy import create_engine, MetaData, Table, Integer, String, \
-    Column, DateTime, ForeignKey, Numeric, PrimaryKeyConstraint, CheckConstraint, \
-    UniqueConstraint
+
+
+from sqlalchemy import Integer, String, Column
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class State(Base):
     '''Class State inheriting from Base'''
-    
+    __tablename__ = 'states'
+    id = Column(Integer(), primary_key=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
