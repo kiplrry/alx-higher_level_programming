6065ad206da1d867a9ee455c82a290a77bd56ce8
kiplrry <kiplarry19@gmail.com>
2023-11-19 21:40:23 +0300

    8-model_state_fetch_first.py

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Querying data from a database using query()"""
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session, sessionmaker
+
+if __name__ == '__main__':
+    engine = create_engine(
+        f'mysql+mysqldb://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}'
+        )
+    # engine = create_engine(
+    # 'mysql+mysqldb://root:pass@localhost:3306/hbtn_0e_6_usa')
+
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+    state = session.query(State).order_by(State.id).first()
+    print(f'{state.id}: {state.name}')
