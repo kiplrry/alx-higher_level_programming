c076d029e46d18f4acfd4522f6afa6b0da3b0dac
kiplrry <kiplarry19@gmail.com>
2023-11-19 21:43:11 +0300

    8-model_state_fetch_first.py

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -16,4 +16,7 @@ if __name__ == '__main__':
 
     session = Session()
     state = session.query(State).order_by(State.id).first()
-    print(f'{state.id}: {state.name}')
+    if state:
+        print(f'{state.id}: {state.name}')
+    else:
+        print()
