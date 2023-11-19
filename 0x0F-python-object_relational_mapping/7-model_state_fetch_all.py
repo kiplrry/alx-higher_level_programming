8ab94da4b92038b671c4eedad68355b54b77b22c
kiplrry <kiplarry19@gmail.com>
2023-11-19 17:42:03 +0300

    7-model_state_fetch_all

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Querying data from a database using query()"""
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session, sessionmaker
+
+if __name__ == '__main__':
+    engine = create_engine(
+        f'mysql+mysqldb://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}'
+        )
+    # engine = create_engine(
+    # f'mysql+mysqldb://root:pass@localhost:3306/hbtn_0e_6_usa')
+
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+    allS = session.query(State).order_by(State.id)
+    for i in allS:
+        print(f'{i.id}: {i.name}')
