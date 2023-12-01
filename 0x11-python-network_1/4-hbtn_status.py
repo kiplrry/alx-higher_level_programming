c7262dd452b1a1c92a6319e10ff0bf1a76ed90ab
kiplrry <kiplarry19@gmail.com>
2023-12-01 23:21:05 +0300

    4-hbtn_status.py

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import requests
+
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    res = requests.get(url)
+
+    print("Body response:")
+    print(f"\t- type: {type(res.text)}")
+    print(f"\t- content: {res.text}")
