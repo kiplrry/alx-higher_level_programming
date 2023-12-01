7df6ed48cf135821ac0ffd103dc8b2c906c871bd
kiplrry <kiplarry19@gmail.com>
2023-12-01 15:15:42 +0300

    0-hbtn_status.pyfix

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,9 +6,7 @@ if __name__ == '__main__':
     req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
     with urllib.request.urlopen(req) as res:
         ans = res.read()
-        print(
-            f"""Body response:
-        - type: {type(ans)}
-        - content: {ans}
-        - utf8 content: {ans.decode('utf-8')}"""
-        )
+        print("Body response:")
+        print(f"\t- type: {type(ans)}")
+        print(f"\t- content: {ans}")
+        print(f"\t- utf8 content: {ans.decode('utf-8')}")
