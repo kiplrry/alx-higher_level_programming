59342be5e812c3e825be439600e180282d6f2f19
kiplrry <kiplarry19@gmail.com>
2023-12-01 15:11:35 +0300

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """python script to get status from an url"""
 import urllib.request
-req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
 
 if __name__ == '__main__':
+    req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
     with urllib.request.urlopen(req) as res:
         ans = res.read()
         print(
