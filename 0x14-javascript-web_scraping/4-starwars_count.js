a6dab18e0616bae9f3798fc6c390cd67c6ef544e
kiplrry <kiplarry19@gmail.com>
2024-02-20 19:40:58 +0300

    4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,12 +3,26 @@
 const request = require('request');
 const id = 18;
 
-const urlpath = `https://swapi-api.alx-tools.com/api/people/${id}/`;
+const urlpath = process.argv[2];
+const person = `https://swapi-api.alx-tools.com/api/people/18/`
+
+
+function count(films) {
+  let num = 0
+  for (const film of films){
+    if (film.characters.includes(person)){
+      num += 1
+    }
+  }
+  return num
+}
+
 
 request(urlpath, (err, res, bdy) => {
   if (err) {
     console.log(err);
   }
   bdy = JSON.parse(bdy);
-  console.log(bdy.films.length);
+  console.log(count(bdy.results))
 });
+
